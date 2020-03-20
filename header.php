5efd6cb5883c603d19fc533b9fc0a26ff7c0d8e9
unlikelyobjects <hello@unlikelyobjects.com>
2020-03-20 11:52:04 +0100

    adding logo

--- header.php
+++ header.php
@@ -26,17 +26,10 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
+			<h1 class="site-title">x<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
+
+			<?
 			// $literarybohemian_description = get_bloginfo( 'description', 'display' );
 			// if ( $literarybohemian_description || is_customize_preview() ) :
 			//	?>
