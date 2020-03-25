5c350248593f805e6f8c43b8cb1aca2a6b90ffff
unlikelyobjects <hello@unlikelyobjects.com>
2020-03-25 17:00:45 +0100

    add primary nav

--- footer.php
+++ footer.php
@@ -15,17 +15,13 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'literarybohemian' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'literarybohemian' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'literarybohemian' ), 'literarybohemian', '<a href="http://colinlewis.se">Colin Lewis</a>' );
-				?>
+			<?php
+			wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+				'menu_id'        => 'primary-menu__footer',
+			) );
+			?>
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
