2e9a14b2a4f4f1fd1d0d3da98144d2b9b514da89
unlikelyobjects <hello@unlikelyobjects.com>
2020-04-03 13:18:40 +0200

    footer menus

--- footer.php
+++ footer.php
@@ -22,10 +22,10 @@
 		<div class="site-info">
 
 				<!-- Footer navigation & copyright -->
-				<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_class' => 'menu-primary-container' ) ); ?>
-				<?php wp_nav_menu( array( 'theme_location' => 'menu-2', 'menu_class' => 'menu-secondary-container' ) ); ?>
-				<?php wp_nav_menu( array( 'theme_location' => 'menu-3', 'menu_class' => 'menu-tertiary-container' ) ); ?>
-				<?php wp_nav_menu( array( 'theme_location' => 'menu-4', 'menu_class' => 'menu-social-channels-container' ) ); ?>
+				<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_class'      => 'footer-menu' ) ); ?>
+				<?php wp_nav_menu( array( 'theme_location' => 'menu-2', 'menu_class'      => 'footer-menu' ) ); ?>
+				<?php wp_nav_menu( array( 'theme_location' => 'menu-3', 'menu_class'      => 'footer-menu' ) ); ?>
+				<?php wp_nav_menu( array( 'theme_location' => 'menu-4', 'menu_class'      => 'footer-menu' ) ); ?>
 
 				<div class="copyright">
 					&copy; 2008–<?php echo date("Y"); ?> The&nbsp;Literary&nbsp;Bohemian<br />
