abea85f47863d3ffd410f21afe081a824b44dfa5
unlikelyobjects <hello@unlikelyobjects.com>
2020-04-29 16:57:32 +0200

    single final 1

--- single.php
+++ single.php
@@ -10,14 +10,16 @@
 get_header();
 ?>
 
+
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<main id="main" class="single-post">
+			<img class="single-post__random-header" src="<?php echo get_template_directory_uri(); ?>/img/single-random-header/1.png" alt="">
 
 		<?php
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part( 'template-parts/content', get_post_type() );
 
 			// the_post_navigation();
 
--- template-parts/content.php
+++ template-parts/content.php
@@ -9,51 +9,79 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				literarybohemian_posted_on();
-				literarybohemian_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
+
+<article class="single-post__article">
+
+	<header class="single-post__header">
+
+		<?php
+		// The title
+		// ----------------------------------------------------------------------------
+		the_title( '<h1 class="single-post__heading">', '</h1>' );
+
+		// Meta
+		// ----------------------------------------------------------------------------
+		echo '<ul class="single-post__meta">';
+
+		// The author
+		echo '<li class="single-post__meta--name">' . get_field('name') . '</li>';
+
+		// Get the custom post type for Journal posts
+		if ( get_post_type( get_the_ID() ) == 'poetry' ) {
+			$cpt = 'Poetry';
+		} elseif ( get_post_type( get_the_ID() ) == 'postcard_prose' ) {
+			$cpt = 'Postcard Prose';
+		} elseif ( get_post_type( get_the_ID() ) == 'travelogue' ){
+			$cpt = 'Travelogue';
+		}
+
+		// The Journal section
+		if ( get_post_type( get_the_ID() ) == 'poetry' or 'postcard_prose' or 'travelogue' ) {
+			echo '<li class="single-post__meta--post-type">' . $cpt . '</li>';
+		}
+
+
+		// The category & tags
+		// ----------------------------------------------------------------------------
+	  $post_id = $post->ID;
+
+		// get the category
+	  $cats = array();
+	  foreach (get_the_category($post_id) as $c) {
+	    $cat = get_category($c);
+	    array_push($cats, $cat->name);
+	  }
+
+	  if (sizeOf($cats) > 0) {
+	    $post_categories = implode(', ', $cats);
+	  } else {
+	    $post_categories = '—'; // If we ever see this, we know there's no assigned category
+	  }
+
+		// Print category and tags
+		echo '<li class="single-post__meta--taxonomy-wrapper">';
+
+		echo '<ul class="single-post__meta--taxonomy">';
+		echo '<li class="single-post__meta--taxonomy-cat">' . $post_categories . '</li>';
+		the_tags( '<li class="single_post__meta--taxonomy-tag">', '</li><li class="single_post__meta--taxonomy-tag">', '</li>' );
+		echo '</ul>';
+
+		echo '</li>';
+		echo '</ul>'; // close single-post__meta
+
+		?>
+
 	</header><!-- .entry-header -->
 
-	<?php literarybohemian_post_thumbnail(); ?>
+	<div class="single-post__body">
 
-	<div class="entry-content">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'literarybohemian' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
+		<?php the_content(); ?>
+		<?php the_field( 'text' ); ?>
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'literarybohemian' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
+	</div><!-- .single-post__content -->
 
-	<footer class="entry-footer">
-		<?php literarybohemian_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+	<!-- <footer class="entry-footer"> -->
+		<?php // literarybohemian_entry_footer(); ?>
+	<!-- </footer><!-- .entry-footer -->
+</article><!-- #post-<?php //the_ID(); ?> -->
