a351ff364896aee4ade99216dec167d58f8c41ae
unlikelyobjects <hello@unlikelyobjects.com>
2020-05-02 10:50:25 +0200

    fixed float label for newsletter

--- template-parts/content-newsletter.php
+++ template-parts/content-newsletter.php
@@ -39,3 +39,26 @@
 		<!-- /newsletter form -->
 
 </div> <!-- .newsletter -->
+
+
+
+
+<!-- <div id="mc_embed_signup">
+	<form action="https://literarybohemian.us3.list-manage.com/subscribe/post?u=e10427dc014f047245e843d80&amp;id=cf83fcce33" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+		<div id="mc_embed_signup_scroll" class="newsletter__form-wrapper">
+			<div class="mc-field-group">
+				<label for="mce-EMAIL">Email Address</label>
+				<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+			</div>
+			<div id="mce-responses" class="clear">
+				<div class="response" id="mce-error-response" style="display:none"></div>
+				<div class="response" id="mce-success-response" style="display:none"></div>
+			</div> -->
+			<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+			<!-- <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_e10427dc014f047245e843d80_cf83fcce33" tabindex="-1" value=""></div>
+			<div class="clear">
+				<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+			</div>
+    </div>
+</form>
+</div> -->
