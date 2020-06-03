ff251530dac9be68b2bc20fe927ae3c153260261
unlikelyobjects <hello@unlikelyobjects.com>
2020-06-03 23:31:02 +0200

    cat description test

--- page-issues.php
+++ page-issues.php
@@ -19,33 +19,34 @@ get_header();
 
       <ul class="issues-list">
           <?php
-							// wp_list_categories( array(
-              // 'orderby'    => 'name',
-              // 'exclude'    => array( 1 ),
-      				// 'title_li' => ''
-          		// ) );
+							wp_list_categories( array(
+              'orderby'    => 'name',
+              'exclude'    => array( 1 ),
+							'use_desc_for_title' => 1,
+      				'title_li' => ''
+          		) );
 					?>
 
 
 <?php
-			$categories = get_categories( array(
-			    'orderby' => 'name',
-					'exclude'    => array( 1 ),
-					'title_li' => ''
-			) );
-
-			foreach( $categories as $category ) {
-			    $category_link = sprintf(
-			        '<li><a href="%1$s" alt="%2$s">%3$s</a></li>',
-			        esc_url( get_category_link( $category->term_id ) ),
-			        esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ),
-			        esc_html( $category->name )
-			    );
+			// $categories = get_categories( array(
+			//     'orderby' => 'name',
+			// 		'exclude'    => array( 1 ),
+			// 		'title_li' => ''
+			// ) );
+			//
+			// foreach( $categories as $category ) {
+			//     $category_link = sprintf(
+			//         '<li><a href="%1$s" alt="%2$s">%3$s</a></li>',
+			//         esc_url( get_category_link( $category->term_id ) ),
+			//         esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ),
+			//         esc_html( $category->name )
+			//     );
 
 			    // echo '<p>' . sprintf( esc_html__( 'Category: %s', 'textdomain' ), $category_link ) . '</p> ';
-			    echo '<p>' . sprintf( esc_html__( 'Description: %s', 'textdomain' ), $category->description ) . '</p>';
+			    // echo '<p>' . sprintf( esc_html__( 'Description: %s', 'textdomain' ), $category->description ) . '</p>';
 			    // echo '<p>' . sprintf( esc_html__( 'Post Count: %s', 'textdomain' ), $category->count ) . '</p>';
-			}
+			// }
 ?>
 
 </ul>
