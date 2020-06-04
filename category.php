49fb49711948282e18e127028e3d917cf47ba46a
unlikelyobjects <hello@unlikelyobjects.com>
2020-06-04 23:43:24 +0200

    revert

--- category.php
+++ category.php
@@ -54,95 +54,152 @@ if (sizeOf($cats) > 0) {
 
 
 			// Issue contents
-			if ($post_categories != "—") {
-				echo '<div class="archived-issue__issue-content highlight-box">';
-				echo '<h2 class="archived-issue__issue-content--heading">' . $post_categories . '</h2>';
-				echo '<h3 class="archived-issue__issue-content--heading-2">Table of contents</h3>';
-				echo '<ul class="archived-issue__issue-content--list">';
+			echo '<div class="archived-issue__issue-content highlight-box">';
+			echo '<h2 class="archived-issue__issue-content--heading">' . $post_categories . '</h2>';
+			echo '<h3 class="archived-issue__issue-content--heading-2">Table of contents</h3>';
+			echo '<ul class="archived-issue__issue-content--list">';
 
-				// ------------------------------
+			// Poetry section
+			// ------------------------------
+			rewind_posts(); // Rewind the loop and start over
 
-				// Get the category title
-				$post_id_0 = $post->ID;
+			while ( have_posts() ) : the_post();
 
-				// get the category
-				$cats = array();
-				foreach (get_the_category($post_id_0) as $c) {
-					$cat = get_category($c);
-					array_push($cats, $cat->name);
-				}
+			// Check for CPT
+			// if ( get_post_type( get_the_ID() ) == 'poetry' ) {
 
-				if (sizeOf($cats) > 0) {
-					$post_categories = implode(', ', $cats);
-				} else {
-					$post_categories = '—';
-				}
-
-				$category_id = get_cat_ID($post_categories);
-
-				// Poetry
-				$args_p = array('category' => $category_id, 'post_type' => 'poetry', 'numberposts' => '100',);
-				$postslist_p = get_posts($args_p);
-				// Postcard Prose
-				$args_pp = array('category' => $category_id, 'post_type' => 'postcard_prose', 'numberposts' => '100',);
-				$postslist_pp = get_posts($args_pp);
-				// Travel Notes
-				$args_t = array('category' => $category_id, 'post_type' => 'travel_notes', 'numberposts' => '100',);
-				$postslist_t = get_posts($args_t);
-
-
-				// Poetry section
-				if ($postslist_p) {
+				// Loop once, then break
+				static $count_p = 0;
+				if ($count_p == "1") { break; }
+				else {
+					// CPT heading
 					echo '<li class="archived-issue__issue-content--list-section-heading">Poetry';
 					echo '<ul class="archived-issue__issue-content--list-section">';
-					// Print the linked poetry titles
-					foreach ($postslist_p as $post) :  setup_postdata($post);
+					$count_p++; }
+				// }
+			endwhile;
 
-						// Check for multiple poems
-						if (get_field('multiple_poems') == 1) :
-							// No name needed.
-							echo '<li class="archived-issue__issue-content--list-section-item"><a class="archived-issue__issue-content--list-section-item-link" href="' . get_the_permalink() . '">' . get_the_title() . '</a></li>';
-						else :
-							$p_name = get_field('name');
-							echo '<li class="archived-issue__issue-content--list-section-item"><a class="archived-issue__issue-content--list-section-item-link" href="' . get_the_permalink() . '">' . get_the_title() . '</a> <span class="inline-byline">By ' . $p_name . '</span></li>';
-						endif;
+			// Get the post links for the section
+			rewind_posts(); // Rewind the loop and start over
+			while ( have_posts() ) : the_post();
 
-					endforeach;
-					echo '</ul>';
-					echo '</li>';
+				// Check for CPT
+				if ( get_post_type( get_the_ID() ) == 'poetry' ) {
+					echo '<li class="archived-issue__issue-content--list-section-item"><a class="archived-issue__issue-content--list-section-item-link" href="' . get_the_permalink() . '">' . get_the_title() . '</a></li>';
 				}
 
-				// Postcard Prose
-				if ($postslist_pp) {
+			endwhile; // End of the loop.
+
+			// Close the tags
+			rewind_posts(); // Rewind the loop and start over
+			while ( have_posts() ) : the_post();
+			if ( get_post_type( get_the_ID() ) == 'poetry' ) {
+				// Loop once, then break
+				static $count_p1 = 0;
+				if ($count_p1 == "1") { break; }
+				else {
+					// CPT heading
+					echo '</ul>';
+					echo '</li>';
+					$count_p1++; }
+				}
+			endwhile;
+
+
+			// Postcard Prose section
+			// ------------------------------
+			rewind_posts(); // Rewind the loop and start over
+			while ( have_posts() ) : the_post();
+
+			// Check for CPT
+			if ( get_post_type( get_the_ID() ) == 'postcard_prose' ) {
+
+				// Loop once, then break
+				static $count_pp = 0;
+				if ($count_pp == "1") { break; }
+				else {
+					// CPT heading
 					echo '<li class="archived-issue__issue-content--list-section-heading">Postcard Prose';
 					echo '<ul class="archived-issue__issue-content--list-section">';
-					// Print the linked Postcard Prose titles
-					foreach ($postslist_pp as $post) :  setup_postdata($post);
-						// Get the author name
-						$pp_name = get_field('name');
-						echo '<li class="archived-issue__issue-content--list-section-item"><a class="archived-issue__issue-content--list-section-item-link" href="' . get_the_permalink() . '">' . get_the_title() . '</a> <span class="inline-byline">By ' . $pp_name . '</span></li>';
-					endforeach;
-					echo '</ul>';
-					echo '</li>';
+					$count_pp++; }
+				}
+			endwhile;
+
+			// Get the post links for the section
+			rewind_posts(); // Rewind the loop and start over
+			while ( have_posts() ) : the_post();
+
+				// Check for CPT
+				if ( get_post_type( get_the_ID() ) == 'postcard_prose' ) {
+					echo '<li class="archived-issue__issue-content--list-section-item"><a class="archived-issue__issue-content--list-section-item-link" href="' . get_the_permalink() . '">' . get_the_title() . '</a></li>';
 				}
 
-				// Travel Notes section
-				if ($postslist_t) {
+			endwhile; // End of the loop.
+
+			// Close the tags
+			rewind_posts(); // Rewind the loop and start over
+			while ( have_posts() ) : the_post();
+			if ( get_post_type( get_the_ID() ) == 'postcard_prose' ) {
+				// Loop once, then break
+				static $count_pp2 = 0;
+				if ($count_pp2 == "1") { break; }
+				else {
+					// CPT heading
+					echo '</ul>';
+					echo '</li>';
+					$count_pp2++; }
+				}
+			endwhile;
+
+
+			// Travel Notes section
+			// ------------------------------
+			rewind_posts(); // Rewind the loop and start over
+			while ( have_posts() ) : the_post();
+
+			// Check for CPT
+			if ( get_post_type( get_the_ID() ) == 'travel_notes' ) {
+
+				// Loop once, then break
+				static $count_tn = 0;
+				if ($count_tn == "1") { break; }
+				else {
+					// CPT heading
 					echo '<li class="archived-issue__issue-content--list-section-heading">Travel Notes';
 					echo '<ul class="archived-issue__issue-content--list-section">';
-					// Print the linked Travel Notes titles
-					foreach ($postslist_t as $post) :  setup_postdata($post);
-						// Get the author name
-						$t_name = get_field('name');
-						echo '<li class="archived-issue__issue-content--list-section-item"><a class="archived-issue__issue-content--list-section-item-link" href="' . get_the_permalink() . '">' . get_the_title() . '</a> <span class="inline-byline">By ' . $t_name . '</span></li>';
-					endforeach;
-					echo '</ul>';
-					echo '</li>';
+					$count_tn++; }
+				}
+			endwhile;
+
+			// Get the post links for the section
+			rewind_posts(); // Rewind the loop and start over
+			while ( have_posts() ) : the_post();
+
+				// Check for CPT
+				if ( get_post_type( get_the_ID() ) == 'travel_notes' ) {
+					echo '<li class="archived-issue__issue-content--list-section-item"><a class="archived-issue__issue-content--list-section-item-link" href="' . get_the_permalink() . '">' . get_the_title() . '</a></li>';
 				}
 
-				echo '</ul>';
-				echo '</div>';
-			}
+			endwhile; // End of the loop.
+
+			// Close the tags
+			rewind_posts(); // Rewind the loop and start over
+			while ( have_posts() ) : the_post();
+			if ( get_post_type( get_the_ID() ) == 'travel_notes' ) {
+				// Loop once, then break
+				static $count_tn2 = 0;
+				if ($count_tn2 == "1") { break; }
+				else {
+					// CPT heading
+					echo '</ul>';
+					echo '</li>';
+					$count_tn2++; }
+				}
+			endwhile;
+
+
+			echo '</ul>';
+			echo '</div>';
 			?>
 
 				</div><!-- .entry-content -->
