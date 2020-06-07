d42627f5f662fd7b4868a66cf90a3269b89ea1be
unlikelyobjects <hello@unlikelyobjects.com>
2020-06-07 23:17:54 +0200

    Contact page template part

---
+++ template-parts/content-page-contact.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template part Contact
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package The_Literary_Bohemian
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+TEST
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<?php literarybohemian_post_thumbnail(); ?>
+
+	<div class="entry-content">
+		<?php
+		the_content();
+
+		// wp_link_pages( array(
+		// 	'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'literarybohemian' ),
+		// 	'after'  => '</div>',
+		// ) );
+		?>
+	</div><!-- .entry-content -->
+
+</article><!-- #post-<?php the_ID(); ?> -->
