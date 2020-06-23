8a227e445572c134de8e0d5945d3a9f75acf9671
unlikelyobjects <hello@unlikelyobjects.com>
2020-06-23 09:33:57 +0200

    update card count

--- template-parts/content-cards.php
+++ template-parts/content-cards.php
@@ -122,7 +122,7 @@ wp_reset_query();
   echo '</div>';
   ?>
 
-    <div class="card__bg" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/random/<?php echo rand(1, 9) ?>.jpg');"></div>
+    <div class="card__bg" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/random/<?php echo rand(1, 26) ?>.jpg');"></div>
 
     <?
 
