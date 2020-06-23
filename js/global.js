279515f67e5ccdc82a997b368b2c85f4667ef2aa
unlikelyobjects <hello@unlikelyobjects.com>
2020-06-23 15:59:55 +0200

    remove dropcap

--- js/global.js
+++ js/global.js
@@ -1,6 +1,5 @@
 // @codekit-prepend quiet "inclusive-cards-min.js";
 // @codekit-append quiet "skip-link-focus-fix-min.js";
-// @codekit-append quiet "dropcap-span.js";
 // @codekit-append quiet "jQuery.succinct-min.js";
 
 // Call jQuery.succinct.js
